289c08cd4eff34e3e9ba1b9f52ccb0952e2aece7
Vaporise <draxdragonfire@googlemail.com>
2024-10-08 02:28:02 +0900

    New code changes and shapes code

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
-        screen.fill(black)
-        display.flip()
-    print('"Starting asteroids!"')
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        screen.fill("black")
+        pygame.display.flip()
+        
+        
+        dt = clock.tick(60) / 1000
+               
 
 if __name__ == "__main__":
     main()
