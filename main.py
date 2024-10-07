a315bc8f0975de509ddc7cdb9460059cea1bbf0a
Vaporise <draxdragonfire@googlemail.com>
2024-10-07 13:50:43 +0900

    Beginning of game coding and setup

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import pygame #Imports the pygame library we want to use.
+from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill(black)
+        display.flip()
     print('"Starting asteroids!"')
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
