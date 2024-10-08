7fdea01038ae0a47914843a05aeab33804bbf707
Vaporise <draxdragonfire@googlemail.com>
2024-10-08 22:48:14 +0900

    Added movement to the player left and right rotations

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import *
-from constants import PLAYER_RADIUS, SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import PLAYER_RADIUS, SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -21,14 +21,29 @@ class Player(CircleShape):
         white = (255,255,255)
         pygame.draw.polygon(screen, white, self.triangle(), 2)
 
+    def rotate(self, dt): #method to rotate player
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)   
+
 def main():
     pygame.init()
     screen= pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #set display
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-   
+    clock = pygame.time.Clock()
+
    #Game loop
     running = True
     while running:
+
+        dt = clock.tick(60) / 1000 # cap frame rate at 60fps and convert ms to seconds
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -39,6 +54,9 @@ def main():
 
         pygame.display.flip()
 
+        player.update(dt)
+
     pygame.quit()
+
 
 main()
