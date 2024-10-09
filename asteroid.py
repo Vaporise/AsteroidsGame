d1065ffaf8b46aa5c87ad4350fd43b5155d6025e
Vaporise <draxdragonfire@googlemail.com>
2024-10-09 23:39:40 +0900

    Added a shot to the code

--- asteroid.py
+++ asteroid.py
@@ -1,22 +1,21 @@
 import pygame
+import random
+from constants import *
 from circleshape import CircleShape
+
+print("Asteroid module loaded")
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
+        print("Asteroid __init__ called")
         super().__init__(x, y, radius)
 
-        self.velocity = pygame.math.Vector2(0, 0)
-
-        self.image = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
-        white = (255, 255, 255)
-        pygame.draw.circle(self.image, white, (radius, radius), radius, 2)
-
-    
-        self.rect = self.image.get_rect(center=(x, y))
-
     def draw(self, screen):
-        screen.blit(self.image, self.rect)
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
-        self.rect.x += self.velocity.x * dt
-        self.rect.y += self.velocity.y * dt
+        self.position += self.velocity * dt
+        super().wrap_position(SCREEN_WIDTH, SCREEN_HEIGHT)
+
+print("Asteroid class defined")
+print(dir(Asteroid))
--- player.py
+++ player.py
@@ -1,16 +1,19 @@
 import pygame
-from circleshape import *
-from constants import PLAYER_RADIUS, SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import *
+from circleshape import CircleShape
 
-
-
-class Player(CircleShape, pygame.sprite.Sprite):
+class Player(CircleShape):
     def __init__(self, x, y):
-        CircleShape.__init__(self,x, y, PLAYER_RADIUS)
-        pygame.sprite.Sprite.__init__(self)
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+    
+    def wrap_position(self, SCREEN_WIDTH, SCREEN_HEIGHT):
+        self.position.x %= SCREEN_WIDTH
+        self.position.y %= SCREEN_HEIGHT
 
-    # in the player class
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -19,63 +22,24 @@ class Player(CircleShape, pygame.sprite.Sprite):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
-    #Override the draw method
-    def draw(self, screen):
-        white = (255,255,255)
-        pygame.draw.polygon(screen, white, self.triangle(), 2)
-
-    def rotate(self, dt): #method to rotate player
-        self.rotation += PLAYER_TURN_SPEED * dt
-
     def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
-        if keys[pygame.K_w]:
-            self.move(dt)
-        if keys[pygame.K_s]:
-            self.move(-dt)    
+   
+        self.wrap_position(SCREEN_WIDTH, SCREEN_HEIGHT)
 
-        
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
-
-def main():
-    pygame.init()
-    screen= pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #set display
-    clock = pygame.time.Clock()
-    updateable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-    Player.containers = (updateable, drawable)
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
-   #Game loop
-    running = True
-    while running:
-
-        dt = clock.tick(60) / 1000 # cap frame rate at 60fps and convert ms to seconds
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        
-        screen.fill((0,0,0))
-
-        updateable.update(dt)
-
-        for sprite in drawable:
-            sprite.draw(screen)
-
-        pygame.display.flip()
-
-        
-
-    pygame.quit()
-
-if __name__ == "__main__":
-    main()
